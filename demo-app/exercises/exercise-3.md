48b952a36c3ee90c647845b754e9a56eac13ed60
Eric Greene <eric@t4d.io>
2021-06-21 19:11:43 -0400

    add exercise 3

---
+++ demo-app/exercises/exercise-3.md
@@ -0,0 +1,16 @@
+# Exercise 3
+
+## Steps
+
+1. Create a car object for each car with the following properties in an array of cars INSIDE of car tool using the data from the car table:
+
+- id
+- make
+- model
+- year
+- color
+- price
+
+2. Dynamically build the table rows with the array of car objects.
+
+3. Ensure it works!
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,16 +1,33 @@
 export const ColorTool = () => {
+  const colors = [
+    { id: 1, name: "green" },
+    { id: 2, name: "blue" },
+    { id: 3, name: "red" },
+  ];
+
+  //   const colorItems = [];
+
+  //   for (let x = 0; x < colors.length; x++) {
+  //     colorItems.push(<li>{colors[x]}</li>);
+  //   }
+
+  //   colors.forEach((color) => {
+  //     console.log(color);
+  //   });
+
+  const fn = (color) => {
+    // key needs to be unique value for each model
+    return <li key={color.id}>{color.name}</li>;
+  };
+
+  const colorItems = colors.map(fn);
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
-      <ul>
-        <li>green</li>
-        <li>blue</li>
-        <li>red</li>
-        <li>yellow</li>
-        <li>gray</li>
-      </ul>
+      <ul>{colorItems}</ul>
     </>
   );
 };
