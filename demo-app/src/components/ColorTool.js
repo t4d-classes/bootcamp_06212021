409eb066dd887cbb5d5aaca9f85c2f2ef23b502d
Eric Greene <eric@t4d.io>
2021-06-22 13:27:16 -0400

    add color form

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,17 +1,60 @@
-export const ColorTool = (props) => {
-  const fn = (color) => {
-    // key needs to be unique value for each model
-    return <li key={color.id}>{color.name}</li>;
+import { useState } from "react";
+
+export const ColorTool = ({ colors }) => {
+  // setColorForm - update the state date, and it will trigger the re-render
+
+  const [
+    colorForm /* state data */,
+    setColorForm /* function to update the state data */,
+  ] = useState(
+    {
+      colorName: "",
+      colorHexcode: "",
+    } /* initial state data */
+  );
+
+  const change = (e) => {
+    setColorForm({
+      ...colorForm,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const colorItems = props.colors.map(fn);
+  console.log(colorForm);
+  console.log("re-rendering");
 
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
-      <ul>{colorItems}</ul>
+      <ul>
+        {colors.map((c) => (
+          <li key={c.id}>{c.name}</li>
+        ))}
+      </ul>
+      <form>
+        <div>
+          <label htmlFor="color-name-input">Color Name</label>
+          <input
+            type="text"
+            id="color-name-input"
+            value={colorForm.colorName}
+            onChange={change}
+            name="colorName"
+          />
+        </div>
+        <div>
+          <label htmlFor="color-hexcode-input">Color Hexcode</label>
+          <input
+            type="text"
+            id="color-hexcode-input"
+            value={colorForm.colorHexcode}
+            onChange={change}
+            name="colorHexcode"
+          />
+        </div>
+      </form>
     </>
   );
 };
