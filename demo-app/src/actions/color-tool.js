71ddd08f5e1d2a4000b942d661de11b63eb028b7
Eric Greene <eric@t4d.io>
2021-06-29 17:31:44 -0400

    add code demos

--- demo-app/src/actions/color-tool.js
+++ demo-app/src/actions/color-tool.js
@@ -1,5 +1,29 @@
+export const REFRESH_COLORS_REQUEST_ACTION = 'REFRESH_COLORS_REQUEST';
+export const REFRESH_COLORS_DONE_ACTION = 'REFRESH_COLORS_DONE';
+
 export const ADD_COLOR_ACTION = 'ADD_COLOR';
 export const DELETE_COLOR_ACTION = 'DELETE_COLOR';
+
+
+export const createRefreshColorsRequestAction = () =>
+  ({ type: REFRESH_COLORS_REQUEST_ACTION });
+
+export const createRefreshColorsDoneAction = (colors) =>
+  ({ type: REFRESH_COLORS_DONE_ACTION, colors });
+
+export const refreshColors = () => {
+
+  return (dispatch) => {
+
+    dispatch(createRefreshColorsRequestAction());
+
+    return fetch("http://localhost:3060/colors")
+      .then(res => res.json())
+      .then(colors => dispatch(createRefreshColorsDoneAction(colors)));
+  };
+
+};
+
 
 export const createAddColorAction = color => ({
   type: ADD_COLOR_ACTION,
--- demo-app/src/stores/colorToolStore.js
+++ demo-app/src/stores/colorToolStore.js
@@ -1,5 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import { colorToolReducer } from '../reducers/color-tool';
 
-export const colorToolStore = createStore(colorToolReducer);
+export const colorToolStore = createStore(
+  colorToolReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
